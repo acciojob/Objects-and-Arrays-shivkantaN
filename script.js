a84ee9ff0df076ad6a29c1f156cf7247e58a8461
Shivkanta Nagargoje <125877363+shivkantaN@users.noreply.github.com>
2023-05-09 16:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+
+const team=players;
+const team1 = players;
+const cap1 = person;
+ 
+
+
+export {players,person,team,team1,cap1}
