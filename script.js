69da69042b692b83e68046dbfc383ac7239624ef
Shivkanta Nagargoje <125877363+shivkantaN@users.noreply.github.com>
2023-05-09 16:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const person = {
       name: 'John Doe',
       age: 80
     };
-const team=players;
-const team1 = players;
-const cap1 = person;
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = players.slice();
 
+//let copiedPerson = Object.assign({}, person);
+let cap1 = Object.assign({}, person);
 
-export {players,person,team,team1,cap1}
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
